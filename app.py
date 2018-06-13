67cedc436fd9c7bec6b7a758ae32ce5b982dc9c0
hsh <hans12312@naver.com>
2018-06-13 21:27:21 +0900

    bank

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ now_user=""
 now_user_id=""
 choice_account=""
 now_accounts=""
+status_login=0
 
 EMAIL_REGEX=re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
 
@@ -37,7 +38,7 @@ class transfer_class:
 
 @app.route('/',methods=['GET'])
 def index():
-	return render_template("login.html")
+	return render_template("/home.html")
 
 @app.route('/register', methods=['GET'])
 def init_register():
@@ -65,48 +66,76 @@ def register():
 		flash('password not match ! ')
 		return redirect('/register')
 
+
 	else:
-		print (request.form['email'])
-		print (type(request.form['email']))
-		new_name=request.form['name']
-		new_Id=request.form['id']
-		new_email=request.form['email']
-		new_password=request.form['password']
-
-		sql="INSERT INTO userinfor (name, id, pw, email) VALUES('{}', '{}','{}','{}')".format(new_name, new_Id, new_password, new_email)
+		sql="SELECT * FROM userinfor WHERE id='{}'".format(request.form['id'])
 		curs.execute(sql)
-		conn.commit();
+		id_check=curs.fetchall()
 
-		return redirect("/")
+		if not id_check :
+			new_name=request.form['name']
+			new_Id=request.form['id']
+			new_email=request.form['email']
+			new_password=request.form['password']
+
+			sql="INSERT INTO userinfor (name, id, pw, email) VALUES('{}', '{}','{}','{}')".format(new_name, new_Id, new_password, new_email)
+			curs.execute(sql)
+			conn.commit();
+			return render_template("home.html")
+		else :
+			flash('can`t use this id')
+			return redirect('/register')
 
 @app.route('/login', methods=['POST'])
 def login():
+	return render_template("/login.html")
+
+
+@app.route('/do_login', methods=['POST'])
+def do_login():
 	login_id=request.form['id']
 	login_password=request.form['password']
 
 	sql="SELECT * FROM userinfor WHERE id='{}' AND pw='{}'".format(login_id, login_password)
-
-	print (sql)
 	curs.execute(sql)
 	login_re=curs.fetchall()
 
 	if not login_re :
 		flash('invaild input ! ')
-		return redirect("/")
+		return render_template("/login.html")
 
 	global now_user
 	global now_user_id
+	global status_login
 	now_user=login_re[0][1]
 	now_user_id=login_re[0][2]
+	status_login=1;
 
-	return render_template('/home.html',now_user_name=now_user)
+	return render_template("/home.html",now_user_name=now_user)
 
 @app.route('/logout', methods=['POST'])
 def logout():
-	return redirect("/")
+	global now_user
+	global now_user_id
+	global choice_account
+	global now_accounts
+	global status_login
+
+	now_user=""
+	now_user_id=""
+	now_accounts=""
+	choice_account=""
+	status_login=0;
+
+	return render_template("/home.html")
 
 @app.route('/mybank', methods=['POST'])
 def mybank():
+
+	global status_login
+	if status_login == 0 :
+		return render_template("/login.html")
+
 	global now_user_id
 	global now_accounts
 	sql="SELECT * FROM accountinfor WHERE id='{}'".format(now_user_id)
@@ -115,7 +144,7 @@ def mybank():
 
 	return render_template("/my_bank.html", now_accounts=now_accounts)
 
-@app.route('/account_work', methods=['GET'])
+@app.route('/account_work', methods=['POST'])
 def account_work():
 	global choice_account
 	choice_account= request.args.get('account')
@@ -136,7 +165,7 @@ def account_work():
 		t_infor=curs.fetchall()
 	
 		for t in t_infor :
-			transfer_infor.append(transfer_class(t[1], "receive", t[5], t[4], t[9]))
+			transfer_infor.append(transfer_class(t[1], "receive", t[3], t[5], t[9]))
 
 		return render_template("/history.html", now_user_name=now_user, choice_account=choice_account, transfer_infor=transfer_infor)
 
@@ -149,17 +178,17 @@ def send_money():
 	curs.execute(sql)
 	check_account=curs.fetchall()
 	if  check_account[0][3] != request.form['password'] :
-		return render_template("transfer.html")
+		return render_template("/transfer.html")
 	
 	if request.form['send_money'] < 1 :
-		return render_template("transfer.html")
+		return render_template("/transfer.html")
 
 	if request.form['receiver_account'] < 1 :
-		return render_template("transfer.html")
+		return render_template("/transfer.html")
 
 	want_send_money=int(request.form['send_money'])
 	if  check_account[0][4] < want_send_money :
-		return render_template("transfer.html")
+		return render_template("/transfer.html")
 
 	receiver_account=request.form['receiver_account']
 
@@ -167,20 +196,15 @@ def send_money():
 	curs.execute(sql)
 	sql_receiver_account=curs.fetchall()
 
-	print (sql_receiver_account)
 
 	if not sql_receiver_account :
 		re_receiver_account=receiver_account
 		re_receiver='don`t know'
 		re_receiver_money=want_send_money
 	else :
-		re_receiver_account=sql_receiver_account[0][2]
-		re_receiver=sql_receiver_account[0][1]
-		re_receiver_money=sql_receiver_account[0][4]+want_send_money
-
-		sql="UPDATE accountinfor SET Money={} WHERE accountid='{}'".format(re_receiver_money, re_receiver_account)
-		curs.execute(sql)
-		conn.commit()
+		re_receiver_account=sql_receiver_account[0][3]
+		re_receiver=sql_receiver_account[0][2]
+		re_receiver_money=sql_receiver_account[0][5]+want_send_money
 
 
 	now=datetime.datetime.now()
@@ -190,28 +214,33 @@ def send_money():
 
 	sql="INSERT INTO transferinfor (date, what, sender, receiver, money, receiver_sum, sender_sum, sender_account, receiver_account)VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(date, 'send',now_user, re_receiver, want_send_money,re_receiver_money, change_money, choice_account, re_receiver_account   )
 	curs.execute(sql)
-	conn.commit()
+	conn.commit();
 
-	sql="UPDATE accountinfor SET money={} WHERE accountid='{}'".format(change_money, choice_account)
+	sql="UPDATE accountinfor SET money={} WHERE accountid={}".format(change_money, choice_account)
 	curs.execute(sql)
-	conn.commit()
+	conn.commit();
 	global now_accounts
 
-	return render_template("my_bank.html",now_accounts=now_accounts)
+	return render_template("/my_bank.html",now_accounts=now_accounts)
 
 @app.route('/go_change_myinfor', methods=['POST'])
 def go_change_myinfor():
-	return render_template("change_myinfor.html")
+	global status_login
+	if status_login == 0 :
+		return render_template("/login.html")
+
+	return render_template("/change_myinfor.html")
 
 @app.route('/change_myinfor', methods=['POST'])
 def change_myinfor():
+
 	if request.form['password'] != request.form['re_password'] :
 		flash('password not match ! ')
-		return render_template("change_myinfor.html")
+		return render_template("/change_myinfor.html")
 
 	if request.form['email'] <1 :
 		flash('email is blank ! ')
-		return render_template("change_myinfor.html")
+		return render_template("/change_myinfor.html")
 
 	new_password=request.form['password']
 	new_email=request.form['email']
@@ -219,14 +248,18 @@ def change_myinfor():
 
 	sql="UPDATE userinfor SET pw='{}', email='{}' WHERE id='{}'".format(new_password, new_email,now_user_id)
 	curs.execute(sql)
-	conn.commit()
+	conn.commit();
 
 	global now_accounts
 
-	return render_template("my_bank.html",now_accounts=now_accounts)
+	return render_template("/my_bank.html",now_accounts=now_accounts)
 
 @app.route('/myloan', methods=['POST'])
 def myloan():
+
+	global status_login
+	if status_login == 0 :
+		return render_template("/login.html")
 	global now_user_id
 
 	global now_loan_accounts
@@ -236,6 +269,30 @@ def myloan():
 
 	return render_template("/my_loan.html", now_loan_accounts=now_loan_accounts)
 
+@app.route('/change_money', methods=['POST'])
+def change_money():
+	return render_template("/change_money.html")
+
+@app.route('/buy_change_money', methods=['POST'])
+def buy_change_money():
+	return render_template("/buy_change_money.html")
+
+
+@app.route('/show_change_money', methods=['POST'])
+def show_change_money():
+	return render_template("/show_change_money.html")
+
+@app.route('/oversea_transfer', methods=['POST'])
+def oversea_transfer():
+	return render_template("/oversea_transfer.html")
+
+
+@app.route('/ok_oversea_transfer', methods=['POST'])
+def ok_oversea_transfer():
+	global status_login
+	if status_login == 0 :
+		return render_template("/login.html")	
+	return render_template("/ok_oversea_transfer.html")
 
 
 if __name__ == "__main__":
